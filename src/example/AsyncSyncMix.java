36f405c39e5eb7acbaad245cc6d48233c0a3185e
coreen <coreeny@cs.washington.edu>
2017-06-22 06:45:25 -0700

    add clearer async/sync mix example and rename existing to be method name tested

--- src/example/AsyncSyncMix.java
+++ src/example/AsyncSyncMix.java
@@ -1,42 +1,31 @@
 package example;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import model.Action;
-import model.Pizza;
-
-/**
- * Composes multiple independent steps together.
- * 
- * @author Coreen Yuen
- */
 public class AsyncSyncMix {
 	public static void main(String[] args) {
-		/*
-		 * .thenComposeAsync() chains the successful results of
-		 * `cookPizza` into a separate nested synchronous function
-		 */
-		// 1. Cook the pizza.
-		CompletableFuture<Pizza> cookPizza = CompletableFuture.supplyAsync(() -> Action.cookPizza());
-		// 2. Action definition for additional pineapple topping.
-		Function<Pizza, CompletableFuture<Pizza>> addPineappleTopping = (pizza) ->
-			CompletableFuture.supplyAsync(() -> pizza.addTopping("pineapple", 1));
-		/*
-		 * Note here that .addTopping() is a synchronous function.
-		 */
-		// 3. Execute that action.
-		CompletableFuture<Pizza> preparedPizza = cookPizza
-			.thenComposeAsync((pizza) -> {
-				CompletableFuture<Pizza> morePineapplesPizza = addPineappleTopping.apply(pizza);
-				// 4. Additional ham topping in a nested fashion.
-				return morePineapplesPizza.supplyAsync(() -> pizza.addTopping("chicken", 1));
-				/*
-				 * Note here that the .thenComposeAsync() treats all the topping
-				 * additions as a single CompletionStage.
-				 */
-			});
-		// 5. Eat it!
-		preparedPizza.thenAcceptAsync(Action::eat).join();
+		List<Integer> integers = new ArrayList<Integer>();
+		integers.add(1);
+		integers.add(2);
+		integers.add(3);
+		
+		integers
+			.stream()
+			.map(integer -> {
+				System.out.println("processing integer: " + integer);
+				return isEven(integer)
+						.thenAccept(result -> {
+							System.out.println("integer is " + (result ? "even" : "odd"));
+						})
+						.join();
+			})
+			.collect(Collectors.toList());
+	}
+
+	private static CompletableFuture<Boolean> isEven(int integer) {
+		return CompletableFuture.completedFuture(integer % 2 == 0);
 	}
 }
